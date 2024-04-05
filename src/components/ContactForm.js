7dc2623faa5530a9a07b1ee4230fb8c565bd639d
RahulBhola <lovebhola8283@gmail.com>
2024-04-05 17:18:09 +0530

    Changed Contact Us from new and improved UI

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,108 +1,152 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import maleUserImage from "../assets/userImage.jpg";
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
+import { FaAddressBook } from "react-icons/fa";
+import { useCookies } from "react-cookie";
+import { makeAuthenticatedGETRequest } from "../utils/serverHelpers";
+import { MdMessage } from "react-icons/md";
 import Navbar from "./footer/FixedFooter";
+import Swal from "sweetalert2";
+import emailjs from "emailjs-com";
+import { useNavigate } from "react-router-dom";
 
-const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    contact: "",
-    message: "",
-  });
+function Profile() {
+  const [user, setUser] = useState(null);
+  const [cookies, ,] = useCookies(["token"]);
+  const [message, setMessage] = useState("");
+  const [, setErrMsg] = useState("");
+  const [, setSuccessMsg] = useState("");
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userData = await makeAuthenticatedGETRequest(
+          "/auth/profileDetails"
+        );
+        setUser(userData);
+      } catch (error) {
+        console.error("Error fetching user profile:", error);
+        // Handle error here
+      }
+    };
 
-  const handleSubmit = (e) => {
+    fetchUserData();
+  }, [cookies.token]);
+
+  const handleUpdate = (e) => {
     e.preventDefault();
-    // Perform actions with form data (e.g., send to a server, display a message)
-    console.log("Form data submitted:", formData);
-    // You can add additional logic here, such as sending the form data to a server
+    if (message === "") {
+      setErrMsg("Message is required!");
+      Swal.fire("warning", "Message is required", "warning");
+    } else {
+      setSuccessMsg(
+        `Thank you dear ${user.firstName} ${user.lastName}, Your Message has been sent successfully!`
+      );
+      Swal.fire(
+        "Success!",
+        "Your Message has been sent successfully. We will contact you soon.",
+        "success"
+      );
+
+      // Construct template parameters
+      const templateParams = {
+        from_name: `${user.firstName} ${user.lastName}`,
+        from_email: user.email,
+        from_contact: user.phone || "",
+        from_address: user.address ? user.address.city : "",
+        message: message,
+      };
+
+      // Send email using EmailJS
+      emailjs
+        .send(
+          "service_arh8zol",
+          "template_m9f1fga",
+          templateParams,
+          "VjyZ53rmx4QfQHBAC"
+        )
+        .then((result) => {
+          console.log(result.text);
+          setSuccessMsg(
+            `Your Message has been sent successfully. We will contact you soon.`
+          );
+          setMessage("");
+        })
+        .catch((error) => {
+          console.error("EmailJS Error:", error);
+        });
+      navigate("/welcome");
+    }
   };
 
   return (
     <div>
-      <div className="flex flex-col mt-[60px]  items-center p-6 h-[650px] bg-sky-800/80 rounded-tr-full rounded-tl-3xl">
-        <img
-          className="rounded-full shadow-md shadow-gray-700"
-          style={{ height: 180, width: 170, marginBottom: 30 }}
-          src="https://img.freepik.com/free-vector/hand-drawn-doctor-answer-questions-clipart-gesture-character_40876-3115.jpg?t=st=1709919298~exp=1709922898~hmac=13038ed9e6870f1206138064379b20411b364da3e4d69dc042465294f218c365&w=740"
-          alt="/"
-        />
-        <h2 className="text-gray-300">Any suggestions</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="m-2">
-            <label className="text-white font-bold" htmlFor="name">
-              Name:
-            </label>
-            <input
-              className="rounded-full p-2 w-[250px] shadow-md shadow-gray-700"
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
+      <div className="flex flex-col items-center justify-center">
+        <div className="font-bold mt-4 text-xl">Contact Us Now</div>
+        <div className="flex relative cursor-pointer">
+          <img
+            src={maleUserImage}
+            alt="User"
+            className="items-center rounded-full w-36 mt-12"
+          />
+        </div>
+      </div>
+      {user && (
+        <div>
+          <div className="flex items-center justify-center border-2 mx-6 mt-6 mb-2 rounded-full shadow-2xl">
+            <FaUserCircle className="text-3xl m-4" />
+            <div className="font-bold text-black pl-4 w-full rounded-r-full">
+              {user.firstName} {user.lastName}
+            </div>
           </div>
-          <div className="m-2">
-            <label className="text-white font-bold" htmlFor="email">
-              Email:
-            </label>
-            <input
-              className="rounded-full p-2 w-[250px] shadow-md shadow-gray-700"
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
+          <div className="flex items-center justify-center border-2 mx-6 mb-2 rounded-full shadow-2xl">
+            <MdAlternateEmail className="text-3xl m-4" />
+            <div className="font-bold text-black pl-4 w-full rounded-r-full">
+              {user.email}
+            </div>
           </div>
-          <div className="m-2">
-            <label className="font-bold text-white" htmlFor="contact">
-              Contact:
-            </label>
-            <input
-              className="rounded-full p-2 w-[250px] shadow-md shadow-gray-700"
-              type="tel"
-              id="contact"
-              name="contact"
-              value={formData.contact}
-              onChange={handleChange}
-              required
-            />
+          {user.phone && (
+            <div className="flex items-center justify-center border-2 mx-6 mb-2 rounded-full shadow-2xl">
+              <FaPhoneSquareAlt className="text-3xl m-4" />
+              <div className="font-bold text-black pl-4 w-full rounded-r-full">
+                {user.phone}
+              </div>
+            </div>
+          )}
+          {user.address && user.address.city && (
+            <div className="flex items-center justify-center border-2 mx-6 mb-2 rounded-full shadow-2xl">
+              <FaAddressBook className="text-3xl m-4" />
+              <div className="font-bold pl-4 w-full rounded-r-full">
+                {user.address.city}
+              </div>
+            </div>
+          )}
+            
+          <div className="flex items-center border-2 mx-6 mt-6 mb-2 rounded-full shadow-2xl">
+            <MdMessage className="text-3xl m-4" />
+            <p className="font-bold m-3">Write Message Below If Any</p>
           </div>
-          <div className="flex flex-col">
-            <label className="font-bold text-white" htmlFor="message">
-              Message:
-            </label>
-            <textarea
-              className="w-30 rounded-lg h-[100px] shadow-md shadow-gray-700"
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            ></textarea>
-          </div>
-          <div className="flex justify-center items-center">
-            <button
-              className="bg-sky-300 px-10 p-2 rounded-lg mt-2"
-              type="submit"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+          <textarea
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            className="m-6 mt-6 rounded-xl shadow-2xl p-4"
+            cols="45"
+            rows="8"
+          ></textarea>
+        </div>
+      )}
+      <div
+        className="flex items-center justify-center mx-6 mb-56 bg-yellow-300 font-bold px-20 py-4 rounded-full mt-6 shadow-xl cursor-pointer hover:bg-yellow-100 space-x-2"
+        onClick={handleUpdate}
+      >
+        Submit
       </div>
       <Navbar />
     </div>
   );
-};
+}
 
-export default ContactForm;
+export default Profile;

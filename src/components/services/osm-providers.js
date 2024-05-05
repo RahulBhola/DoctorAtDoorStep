e32d9309687913389de5c33f5722854313f299ab
RahulBhola <lovebhola8283@gmail.com>
2024-05-05 13:05:48 +0530

    updated unused error

--- src/components/services/osm-providers.js
+++ src/components/services/osm-providers.js
@@ -1,7 +1,11 @@
-export default {
+// Assign the configuration object to a variable
+const mapTilerConfig = {
   maptiler: {
     url: "https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=Ykcf2eyYuRUReyyLwfnv",
-    attribution:
-      '&copy; <a href="https://rahulbhola.github.io/MyPortfolio/">RahulBhola</a>',
+    attribution: '&copy; <a href="https://rahulbhola.github.io/MyPortfolio/">RahulBhola</a>',
   },
 };
+
+// Export the variable as the default export
+export default mapTilerConfig;
+
